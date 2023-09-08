919d19d4fdb3ac66098efc0cf9147f377c83edf5
OTende <mezencev_e@inbox.ru>
2023-09-08 07:08:16 +0700

    add hotel layout

--- feature/hotel/src/main/java/com/example/hotel/presentation/fragment/HotelFragment.kt
+++ feature/hotel/src/main/java/com/example/hotel/presentation/fragment/HotelFragment.kt
@@ -2,13 +2,13 @@ package com.example.hotel.presentation.fragment
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.core.domain.util.Status
-import com.example.hotel.R
+import com.example.hotel.databinding.FragmentHotelBinding
 import com.example.hotel.di.DaggerFeatureHotelComponent
 import com.example.hotel.presentation.viewmodel.HotelViewModel
 import javax.inject.Inject
@@ -17,35 +17,40 @@ class HotelFragment : Fragment() {
     @Inject
     lateinit var viewModel: HotelViewModel
 
+    private var _binding: FragmentHotelBinding? = null
+    private val binding get() = _binding!!
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
-//        (requireActivity().applicationContext as Asd).
-//        (requireActivity().applicationContext as MusicApplication).appComponent.inject(this)
-//        FeatureHotelModule.Bui
         DaggerFeatureHotelComponent.builder().build().inject(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_hotel, container, false)
-        val asd = view.findViewById<TextView>(R.id.asd)
+    ): View {
+        _binding = FragmentHotelBinding.inflate(inflater, container, false)
         viewModel.updateHotel()
         viewModel.hotel.observe(viewLifecycleOwner) { result ->
             when (result.status) {
                 Status.SUCCESS -> {
-                    asd.text = result.data?.name
+//                    binding.asd.text = result.data?.name
                 }
 
                 Status.ERROR -> {
-                    asd.text = result.message
+//                    binding.asd.text = result.message
                 }
                 Status.LOADING -> {
-                    asd.text = "is loading"
+//                    binding.asd.text = "is loading"
                 }
+//                binding.hotelRatingCv.findViewById<TextView>()
             }
         }
-        return view
+        return binding.root
     }
 }
