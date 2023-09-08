c5a99d2a1f5ec672452ea02dc022b542eadfcbd7
OTende <mezencev_e@inbox.ru>
2023-09-08 15:15:16 +0700

    add booking info output

--- feature/booking/src/main/java/com/example/booking/presentation/fragment/BookingFragment.kt
+++ feature/booking/src/main/java/com/example/booking/presentation/fragment/BookingFragment.kt
@@ -1,17 +1,64 @@
 package com.example.booking.presentation.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.booking.R
+import com.example.booking.databinding.FragmentBookingBinding
+import com.example.booking.di.DaggerFeatureHotelComponent
+import com.example.booking.presentation.viewmodel.BookingViewModel
+import com.example.core.domain.util.Status
+import javax.inject.Inject
 
 class BookingFragment : Fragment() {
+    @Inject
+    lateinit var viewModel: BookingViewModel
+
+    private var _binding: FragmentBookingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        DaggerFeatureHotelComponent.builder().build().inject(this)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_booking, container, false)
+    ): View {
+        _binding = FragmentBookingBinding.inflate(inflater, container, false)
+        viewModel.updateBookingInfo()
+        viewModel.bookingInfo.observe(viewLifecycleOwner) { value ->
+            when (value.status) {
+                Status.SUCCESS -> {
+                    value.data?.let {
+                        with (binding) {
+                            flightFromTv.text = it.departure
+                            destinationTv.text = it.arrivalCountry
+                            datesTv.text = getString(R.string.dates_window, it.tourDateStart, it.tourDateStop)
+                            nightsNumberTv.text = it.nightsNumber.toString()
+                            hotelTv.text = it.hotelName
+                            roomTv.text = it.room
+                            foodTv.text = it.nutrition
+                        }
+                    }
+                }
+                Status.LOADING -> {
+
+                }
+                Status.ERROR -> {
+
+                }
+            }
+        }
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ feature/booking/src/main/java/com/example/booking/presentation/viewmodel/BookingViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.booking.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.booking.data.model.BookingInfo
+import com.example.booking.domain.repository.BookingRepository
+import com.example.core.domain.util.Resource
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class BookingViewModel @Inject constructor(private val repository: BookingRepository) : ViewModel() {
+    class Factory @Inject constructor(private val repository: BookingRepository) :
+        ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(BookingViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return BookingViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+
+    private val _bookingInfo = MutableLiveData<Resource<BookingInfo>>()
+    val bookingInfo: LiveData<Resource<BookingInfo>> = _bookingInfo
+
+    fun updateBookingInfo() {
+        viewModelScope.launch {
+            _bookingInfo.postValue(repository.getBookingInfo())
+        }
+    }
+
+    fun addTourist() {
+
+    }
+}
